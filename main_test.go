36d5186476c1a77bf641371ae1b05749dada392b
Dafydd James <dafydd.james@ft.com>
2016-06-03 13:56:14 +0100

    Extend the happy path test to actually look inside the tarball and compare the file contents to the expected data. This is now a decent test.

--- main_test.go
+++ main_test.go
@@ -7,10 +7,14 @@ import (
 	"os"
 	"path/filepath"
 	log "github.com/Sirupsen/logrus"
+	"bytes"
+	"compress/gzip"
+	"archive/tar"
+	"strings"
 )
 
 type mockWriteCloser struct {
-
+	buffer bytes.Buffer
 }
 
 func (wc *mockWriteCloser) Close() error {
@@ -18,10 +22,10 @@ func (wc *mockWriteCloser) Close() error {
 }
 
 func (wc *mockWriteCloser) Write(input []byte) (int, error) {
-	return len(input), nil
+	return wc.buffer.Write(input)
 }
 
-func TestEndToEndProcess(t *testing.T) {
+func TestEndToEndProcessHappyPath(t *testing.T) {
 	assert := assert.New(t)
 	mockFleet := &mockFleetAPI{}
 	mockWriter := &mockWriteCloser{}
@@ -38,4 +42,27 @@ func TestEndToEndProcess(t *testing.T) {
 	filename := "test1.txt"
 	ioutil.WriteFile(filepath.Join(sourceDirPath, filename), []byte(fileContents), 0666)
 	assert.NoError(runInner(mockFleet, mockWriter, sourceDirPath + string(os.PathSeparator), targetDirPath, "mockenv"))
+	assert.NotZero(mockWriter.buffer.Len())
+
+	gzipReader, err := gzip.NewReader(&mockWriter.buffer)
+	assert.NoError(err)
+	tarReader := tar.NewReader(gzipReader)
+	for {
+		header, err := tarReader.Next()
+		if err != nil {
+			break
+		}
+		buf, err := ioutil.ReadAll(tarReader)
+		if err != nil {
+			t.Fatal("There was a problem reading from the tarball")
+		}
+		switch {
+			case strings.HasSuffix(header.Name, "test1.txt"):
+				assert.Equal(buf, []byte(fileContents))
+			default:
+				t.Fatalf("Unknown entry encountered inside tarball: %s", header.Name)
+		}
+	}
 }
+
+// TODO test error cases to improve the coverage
